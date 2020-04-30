4c0ac12f00079e104baa20862377c272e1b4dd9f
yoni_Sku <yoni.sku@gmail.com>
2020-04-30 16:07:07 +0300

    Copied the rss template and renamed to 'TheMarker' with correct dependencies.

---
+++ TheMarker/Entities/TheMarkerRssChannel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace TheMarker.Entities
+{
+    [XmlRoot(ElementName="channel")]
+    public class TheMarkerRssChannel
+    {
+        [XmlElement(ElementName="title")]
+        public string Title { get; set; }
+        
+        [XmlElement(ElementName="link")]
+        public string Link { get; set; }
+        
+        [XmlElement(ElementName="copyright")]
+        public string Copyright { get; set; }
+        
+        [XmlElement(ElementName="language")]
+        public string Language { get; set; }
+        
+        [XmlElement(ElementName="pubDate")]
+        public string PublishDate { get; set; }
+        
+        [XmlElement(ElementName="lastBuildDate")]
+        public string LastBuildDate { get; set; }
+
+        [XmlElement(ElementName = "image")]
+        public TheMarkerRssImage Image { get; set; }
+
+        [XmlElement(ElementName="item")]
+        public List<TheMarkerRssItem> Items { get; set; }
+    }
+}
---
+++ TheMarker/News/TheMarkerProvider.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Content.Api;
+using Extensions;
+using TheMarker.Entities;
+
+namespace TheMarker.News
+{
+    public class TheMarkerProvider : ILatestNewsProvider
+    {
+        private readonly RssFeedProvider _rss;
+        
+        public TheMarkerProvider(HttpClient httpClient = null)
+        {
+            httpClient ??= new HttpClient();
+            
+            _rss = new RssFeedProvider(
+                httpClient,
+                "https://www.TheMarker.co.il/GeneralRSS/0,16335,L-8,00.xml");
+        }
+        
+        public Task<IEnumerable<NewsItem>> GetNews(
+            CancellationToken cancellationToken = default)
+        {
+            return _rss.GetNews(
+                cancellationToken,
+                DeserializeItems);
+        }
+
+        public static IEnumerable<NewsItem> DeserializeItems(Stream xml)
+        {
+            return ToNewsItems(
+                DeserializeXml(xml));
+        }
+        
+        private static TheMarkerRssFeed DeserializeXml(Stream xml)
+        {
+            var serializer = new XmlSerializer(typeof(TheMarkerRssFeed));
+
+            return (TheMarkerRssFeed) serializer.Deserialize(xml);
+        }
+        
+        private static IEnumerable<NewsItem> ToNewsItems(TheMarkerRssFeed feed)
+        {
+            return feed.Channel.Items
+                .Select(NewsItemFactory.Create);
+        }
+    }
+
+}
