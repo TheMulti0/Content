4dde9bf7eb18617a45e1794de90c6740ea68ff3b
TheMulti0 <multi@codeprecise.com>
2020-05-03 17:22:59 +0300

    Added time parser to Galatz

--- Galatz/News/NewsItemFactory.cs
+++ Galatz/News/NewsItemFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Content.Api;
 using Galatz.Entities;
 
@@ -6,17 +8,32 @@ namespace Galatz.News
 {
     public static class NewsItemFactory
     {
+        private const string TimePattern = "[0-9]{4}";
+        
         public static NewsItem Create(Hashtag hashtag, Seo seo)
         {
+            var fullDate = DateTime.Parse(GetFullDate(hashtag));
+
             return new NewsItem(
                 NewsSource.Galatz,
                 hashtag._Hashtag,
                 null,
                 AuthorFactory.Create(hashtag, seo),
-                DateTime.Parse(hashtag.Date),
+                fullDate,
                 GalatzConstants.ToAbsoluteUrl(hashtag.Url),
                 GalatzConstants.ToAbsoluteUrl(hashtag.Img),
                 null);
+        }
+
+        private static string GetFullDate(Hashtag hashtag)
+        {
+            string time = Regex.Match(hashtag.Url, TimePattern, RegexOptions.RightToLeft).Groups.FirstOrDefault()?.Value;
+            
+            string formattedTime = time?.Insert(2, ":"); // Add hour:second seperator
+
+            return formattedTime == null 
+                ? hashtag.Date 
+                : $"{hashtag.Date} {formattedTime}";
         }
     }
 }
