f26e950ede098da3ab9b2795ea29de16536d0a05
TheMulti0 <multi@codeprecise.com>
2020-05-11 13:42:26 +0300

    KanReports

---
+++ Kan/Reports/NewsItemFactory.cs
@@ -0,0 +1,21 @@
+﻿using Content.Api;
+using TwitterScraper;
+
+namespace Kan.Reports
+{
+    public static class NewsItemFactory
+    {
+        public static INewsItem Create(Tweet tweet)
+        {
+            return new NewsItem(
+                NewsSource.KanReports,
+                tweet.Text,
+                null,
+                AuthorFactory.Create(tweet.Author),
+                tweet.PublishDate,
+                tweet.Url,
+                null,
+                null);
+        }
+    }
+}
