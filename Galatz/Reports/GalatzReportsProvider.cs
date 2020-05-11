ad1092b9e7126d55e727384c2c3bb04d465c58a0
TheMulti0 <multi@codeprecise.com>
2020-05-11 10:10:16 +0300

    Added GalatzReports

---
+++ Galatz/Reports/GalatzReportsProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Content.Api;
+using TwitterScraper;
+
+namespace Galatz.Reports
+{
+    public class GalatzReportsProvider : ILatestNewsProvider
+    {
+        private const string Username = "@GLZRadio";
+        private ITwitter _twitter;
+
+        public GalatzReportsProvider(
+            HttpClient client = null)
+        {
+            _twitter = new Twitter(client);
+        }
+
+        public async Task<IEnumerable<INewsItem>> GetNews(CancellationToken cancellationToken = default)
+        {
+            IEnumerable<Tweet> tweets = await _twitter.GetTweets(Username, token: cancellationToken);
+            
+            return tweets.Select(NewsItemFactory.Create);
+        }
+    }
+}
---
+++ Galatz/Reports/NewsItemFactory.cs
@@ -0,0 +1,21 @@
+﻿using Content.Api;
+using TwitterScraper;
+
+namespace Galatz.Reports
+{
+    public static class NewsItemFactory
+    {
+        public static INewsItem Create(Tweet tweet)
+        {
+            return new NewsItem(
+                NewsSource.GalatzReports,
+                tweet.Text,
+                null,
+                AuthorFactory.Create(tweet.Author),
+                tweet.PublishDate,
+                tweet.Url,
+                null,
+                null);
+        }
+    }
+}
